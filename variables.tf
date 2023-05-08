d98510ee5053408563b70816951f4572dbbf1f03
Ahmed Mansour <ahmed_mansour_95@outlook.com>
2023-05-08 14:19:37 +0300

    Declares provider variables in variables.tf file

--- variables.tf
+++ variables.tf
@@ -3,6 +3,10 @@
 
 variable "tenancy_ocid" {}
 variable "region" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "user_ocid" {}
+
 variable "ssh_public_key" {
   default = ""
 }
