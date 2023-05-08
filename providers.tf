542efbc9aeee15b289287f5db8414167187a136d
Ahmed Mansour <ahmed_mansour_95@outlook.com>
2023-05-08 13:55:59 +0300

    Updates providers.tf to have the complete variables

--- providers.tf
+++ providers.tf
@@ -3,8 +3,11 @@
 
 
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  region       = var.region
+region = var.region
+tenancy_ocid = var.tenancy_ocid
+user_ocid = var.user_ocid
+fingerprint = var.fingerprint
+private_key_path = var.private_key_path
 }
 
 provider "oci" {
